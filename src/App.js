49bfab47d8d9b9894028f5c10be6d35330581b5e
Rudraprakash003 <rudraprakash999@gmail.com>
2024-05-22 10:05:04 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './Components/NavBar'
 import { useState } from 'react';
 import ProductsData from './data/ProductsData';
